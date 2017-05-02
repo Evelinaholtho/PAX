3de3d913d58c1cfae1721c3826975a334543b944
emiahn <gqK-H=$Y:1!a$u&W2dYD>
2017-05-02 13:45:32 +0200

    Testkommentar!

--- app/src/main/java/com/example/evelina/pax/Login.java
+++ app/src/main/java/com/example/evelina/pax/Login.java
@@ -3,6 +3,8 @@ package com.example.evelina.pax;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Kommentar!
+
 public class Login extends AppCompatActivity {
 
     @Override
