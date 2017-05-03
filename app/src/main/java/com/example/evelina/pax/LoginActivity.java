43808ab4218598018f0f934fbdf46bb325608b20
Evelina <Evelinahoserlund@gmail.com>
2017-05-03 09:46:22 +0200

    länkat ihop login och home

--- app/src/main/java/com/example/evelina/pax/LoginActivity.java
+++ app/src/main/java/com/example/evelina/pax/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.evelina.pax;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -144,6 +145,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
+        Intent intent = new Intent(this, Home.class);
         if (mAuthTask != null) {
             return;
         }
@@ -187,6 +189,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            startActivity(intent);
         }
     }
 
@@ -233,6 +236,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // and hide the relevant UI components.
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+
         }
     }
 
