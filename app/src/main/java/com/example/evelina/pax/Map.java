f242d3570465828ffdfca6872f65c8df4e58257c
Evelina <Evelinahoserlund@gmail.com>
2017-05-04 16:31:14 +0200

    ..

--- app/src/main/java/com/example/evelina/pax/Map.java
+++ app/src/main/java/com/example/evelina/pax/Map.java
@@ -1,10 +1,14 @@
 package com.example.evelina.pax;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Map extends AppCompatActivity {
 
@@ -21,11 +25,19 @@ public class Map extends AppCompatActivity {
                 selectFragment(item);
                 return true;
             }
-        });    }
+
+
+        });
+
+
+
+    }
 
     private void selectFragment(MenuItem item) {
         ActivitySwitcher.switchActivity(this, item);
     }
+
+
 }
 
 
