5bb5a2cbfe1e29827e3d6a388e948e7bf12b2718
Evelina <Evelinahoserlund@gmail.com>
2017-05-10 09:03:35 +0200

    rundat kanter

--- app/src/main/java/com/example/evelina/pax/CalendarDayview.java
+++ app/src/main/java/com/example/evelina/pax/CalendarDayview.java
@@ -2,6 +2,11 @@ package com.example.evelina.pax;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
 
 public class CalendarDayview extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class CalendarDayview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_dayview);
+
+
+        final Button button = (Button) findViewById(R.id.button08);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
     }
-}
+
+
+
+    }
+
