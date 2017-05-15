4b4bd8c22d9b7622d905b6cb946535f2406e16d3
Evelina <Evelinahoserlund@gmail.com>
2017-05-15 13:34:17 +0200

    Fixat i actionbaren så att logga och text följer med upp till actionbar

--- app/src/main/java/com/example/evelina/pax/Search.java
+++ app/src/main/java/com/example/evelina/pax/Search.java
@@ -33,7 +33,7 @@ public class Search extends AppCompatActivity {
             }
 
         });
-        editText = (EditText) findViewById(R.id.editText);
+        //editText = (EditText) findViewById(R.id.editText);
        // editText.setError("Under utveckling");
 
     }
